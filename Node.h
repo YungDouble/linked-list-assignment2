453d0b2ad89cbde1eb5daf4cb546d63edfcb023b
ezkl <therealdavos@gmail.com>
2024-10-05 11:48:10 -0500

    Initial commit

---
+++ Node.h
@@ -0,0 +1,13 @@
+#ifndef NODE_H
+#define NODE_H
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Constructor to initialize a node with a given value
+    Node(int value);
+};
+
+#endif //NODE_H
