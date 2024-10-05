2f2fcc9e9711ebb06461c5b4c38172cf7811e481
ezkl <therealdavos@gmail.com>
2024-10-05 11:42:58 -0500

    Initial commit

---
+++ LinkedList.h
@@ -0,0 +1,31 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "Node.h"   // Include Node definition
+
+class LinkedList {
+public:
+    // Pointer to the head of the linked list
+    Node* head;
+
+    // Constructor to initialize the linked list
+    LinkedList();
+
+    // Helper function to check if a position is valid
+    bool isValidPosition(int position);
+
+    // FUnction to insert a node at the beginning of the linked list
+    void insertAtBeginning(int value);
+
+    // FUnction to insert a node at the end of the linked list
+    void insertAtEnd(int value);
+
+    // Function to insert a node at a specific position in the linked list
+    void insertAtPosition(int position, int value);
+
+    // Function to print the linked list
+    void printList();
+
+};
+
+#endif //LINKEDLIST_H
